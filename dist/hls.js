6f18572f82b855951225aeda2e20ae85d3359525
Michal Morvay <michal.morvay@accenture.com>
2019-09-19 13:25:08 +0200

    build of fix

--- dist/hls.js
+++ dist/hls.js
@@ -576,6 +576,7 @@ module.exports = lerp
             this.draw(data[i], baseCanvasCtx, 'border')
             // end of emergency event
           } else if (eedEnd > 0 && (i === eedEnd || i === data.length -1)) {
+            eedEnd = 0;
             this.draw(data[i], baseCanvasCtx, 'line')
             // no EE just draw as normally
           } else {
